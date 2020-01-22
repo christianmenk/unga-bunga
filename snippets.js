7389866afe699f35996b4c20bbf3f2e871875860
Christian Menk <christian.menk@ibm.com>
2020-01-22 10:57:49 -0500

    Matching strings, default if one match

---
+++ snippets.js
@@ -0,0 +1,56 @@
+
+const bungas = {
+"async-function": 
+`
+await functionName(parameter);
+
+async functionName(parameter) {
+    return new Promise(async function (resolve, reject) {
+        if(parameter){
+            resolve();
+        } else {
+            reject();
+        }
+    })
+}
+`,
+"api-get":
+`
+export function getAPI(parameter) {
+    return function (dispatch) {
+        let url = '/api/name/' + parameter;
+        axios.get(url)
+            .then(function (response) {
+                if (response.data.status == "success"){
+                    // Successful actions
+                } else if (response.data.status == "error"){
+                    // Error actions
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+`,
+"api-post":
+`
+export function postAPI(data) {
+    return function (dispatch) {
+        axios.post('/api/name', data)
+            .then(function (response) {
+                if (response.data.status == "success"){
+                    // Success actions
+                } else if (response.data.status == "error"){
+                    // Error actions
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+`
+}
+
+module.exports = bungas
--- ungaBunga.js
+++ ungaBunga.js
@@ -2,33 +2,58 @@
 
 require('manakin').global;
 var cb = require("copy-paste");
-
 const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-let bungas = require("./bungas")
-let unga = process.argv[2]
 
-if (unga) {
-    let UNGA_ATTEMPT = null;
-    UNGA_ATTEMPT = bungas[unga.toLowerCase()]
-    if (UNGA_ATTEMPT) {
-        console.info(unga)
-        console.warn(UNGA_ATTEMPT)
+const snippets = require("./snippets")
+const snippetKeys = Object.keys(snippets)
+let input = process.argv[2]
+
+if (input) {
+    input = input.toLowerCase()
+    let UNGA_MATCH  = snippets[input]
+    if (UNGA_MATCH) {
+        printSnippet(input, UNGA_MATCH)
+    } else {
+        giveMeNames(input)
+    }
+} else {
+    giveMeNames()
+}
+
+function giveMeNames(input) {
+    let matchedSnippets = []
+    if (input) {
+        for (let key of snippetKeys) {
+            if (key.includes(input)) {
+                matchedSnippets.push(key)
+            }
+        }
+        if(matchedSnippets.length == 1){
+            printSnippet(matchedSnippets[0], snippets[matchedSnippets[0]])
+        } else {
+            printMatches(matchedSnippets)
+        }
+    } else {
+        printMatches(snippetKeys)
+    }
+}
+
+function printMatches(matches){
+    console.info(matches.join("    "))
+}
+
+function printSnippet(input, UNGA_MATCH){
+        console.info(input)
+        console.warn(UNGA_MATCH)
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
         rl.question("Unga bunga copy code? (y/n): ", function (copy) {
-            if(copy == "y"){
-                cb.copy(UNGA_ATTEMPT)
-                console.success("Copied to Clipboard.")
-                
+            if (copy == "y") {
+                cb.copy(UNGA_MATCH)
+                console.success("Unga bunga copied code.")
             } 
             rl.close()
         });
-    } else {
-        console.log("giveMeNames")
-    }
-} else {
-    console.log("giveMeNames")
-
 }
